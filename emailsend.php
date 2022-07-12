95085172cc76e9d144435d679407c6fcb7dcbdfc
vasu1234118 <83013761+vasu1234118@users.noreply.github.com>
2022-07-12 16:24:28 +0530

    Create emailsend.php

---
+++ emailsend.php
@@ -0,0 +1,34 @@
+function curls($to='', $subject='',$body='' ){
+    
+ //extract($_GET);
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "http://dqci.in/bbj/mymail",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "to=$to&subject=$subject&body=htmlentities($body)",
+  CURLOPT_HTTPHEADER => array(
+    
+    "cache-control: no-cache",
+    "content-type: application/x-www-form-urlencoded",
+    
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+}
